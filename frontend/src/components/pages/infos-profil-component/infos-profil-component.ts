2f93bd2ba1421835689bc9b67d0452c6e67dc913
Gthierry <thierrygillot@live.com>
2025-11-07 10:13:04 +0100

    feat: enhance user profile components with dynamic data binding and improved navigation

--- frontend/src/components/pages/infos-profil-component/infos-profil-component.ts
+++ frontend/src/components/pages/infos-profil-component/infos-profil-component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from '../../../core/models/user-models/user.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'infos-profil',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './infos-profil-component.html',
-  styleUrl: './infos-profil-component.css'
+  styleUrl: './infos-profil-component.css',
 })
 export class InfosProfilComponent {
+  user: User | undefined;
 
+  constructor() {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      this.user = JSON.parse(userData);
+    }
+  }
 }
--- frontend/src/components/pages/user-profil-component/user-profil-component.ts
+++ frontend/src/components/pages/user-profil-component/user-profil-component.ts
@@ -3,29 +3,28 @@ import { User } from '../../../core/models/user-models/user.model';
 import { TokenService } from '../../../core/services/token-service/token-service';
 import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
-
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'user-profil-component',
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './user-profil-component.html',
   styleUrl: './user-profil-component.css',
 })
 export class UserProfilComponent {
-  user: User | null =null
-  route = inject(Router)
-  
+  user: User | null = null;
+  route = inject(Router);
 
-  constructor(tokenService:TokenService) {
-    if(tokenService.getToken())
-    {
+  constructor(tokenService: TokenService) {
+    if (tokenService.getToken()) {
       this.user = JSON.parse(localStorage.getItem('user') || '{}');
       console.log('User from localStorage:', this.user);
-    }
-    else
-    {
+    } else {
       this.route.navigate(['register']);
     }
+  }
+
+  navigateToInfosProfile() {
+    this.route.navigate(['infos-profil']);
   }
 }
