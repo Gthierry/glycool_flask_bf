66686538d7deb77c5d19898ca6b499a6c6af4d15
Gthierry <thierrygillot@live.com>
2025-11-12 13:42:28 +0100

    feat: add footer component and integrate into app layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,17 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "../components/shared/navbar-component/navbar-component";
-
-
+import { NavbarComponent } from '../components/shared/navbar-component/navbar-component';
+import { FooterComponent } from '../components/shared/footer-component/footer-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('glycool-angular');
-
-
 }
