5ede747b9f899e7e0999f7275d5ddeb1ecc2575b
Gthierry <thierrygillot@live.com>
2025-11-16 21:28:10 +0100

    feat(contacts): implement ContactService for contact management operations

---
+++ backend/app/services/contact_service.py
@@ -0,0 +1,38 @@
+from app.dtos.contact_dto import ContactDto
+from app.schemas.contact_insert_schema import ContactInsertSchema
+from app.models.contact import Contact
+from app import db
+
+
+class ContactService:
+
+    @staticmethod
+    def create_contact(data: dict) -> ContactDto:
+
+        contact_data = data.copy()
+
+        contact = Contact(**contact_data)
+        db.session.add(contact)
+        db.session.commit()
+        db.session.refresh(contact)
+        return ContactDto(contact)
+
+    @staticmethod
+    def get_contacts_user_id(user_id: int) -> ContactDto | None:
+
+        contact = Contact.query.filter_by(user_id=user_id).first()
+        if contact:
+            return ContactDto(contact)
+        return None
+
+    @staticmethod
+    def delete_contact(user_id: int, contact_id: int) -> bool:
+
+        contact = Contact.query.filter_by(
+            user_id=user_id, contact_user_id=contact_id
+        ).first()
+        if contact:
+            db.session.delete(contact)
+            db.session.commit()
+            return True
+        return False
