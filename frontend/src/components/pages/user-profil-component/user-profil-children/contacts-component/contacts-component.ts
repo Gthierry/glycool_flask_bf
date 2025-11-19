d6f4ef0e35470139f19fcdb1b1a3fb55a1cead56
Gthierry <thierrygillot@live.com>
2025-11-19 10:17:18 +0100

    feat(contacts): implement contact retrieval for authenticated users

--- frontend/src/components/pages/user-profil-component/user-profil-children/contacts-component/contacts-component.ts
+++ frontend/src/components/pages/user-profil-component/user-profil-children/contacts-component/contacts-component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { AuthService } from '../../../../../core/services/authentification/auth-service';
+import { ContactService } from '../../../../../core/services/contact-service/contact-service';
+import { Contact } from '../../../../../core/models/contact-models/contact.model';
+import { User } from '../../../../../core/models/user-models/user.model';
 
 @Component({
   selector: 'app-contacts-component',
@@ -7,5 +11,29 @@ import { Component } from '@angular/core';
   styleUrl: './contacts-component.css',
 })
 export class ContactsComponent {
+  authService = inject(AuthService);
+  userSignal = this.authService.userSignal;
+  contactService = inject(ContactService);
+  user: User | null = null;
 
+  contactsList: Contact[] = [];
+
+  constructor() {
+    //Utiliser effect pour réagir aux changements du signal
+    effect(() => {
+      const userData = this.userSignal();
+      if (userData) {
+        this.user = userData;
+        this.contactService.getAllContactsForUser(this.user).subscribe({
+          next: (response) => {
+            this.contactsList = response;
+            console.log('Contacts récupérés !');
+          },
+          error() {
+            console.log('Erreur de récupération de contacts');
+          },
+        });
+      }
+    });
+  }
 }
