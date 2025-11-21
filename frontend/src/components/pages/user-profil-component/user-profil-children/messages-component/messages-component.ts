f839138e9e7993855b701476cddd5f9b69ef0fbd
Gthierry <thierrygillot@live.com>
2025-11-21 12:19:49 +0100

    feat(messages): update message display and add delete functionality

--- frontend/src/components/pages/user-profil-component/user-profil-children/messages-component/messages-component.ts
+++ frontend/src/components/pages/user-profil-component/user-profil-children/messages-component/messages-component.ts
@@ -1,6 +1,9 @@
 import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MessageJson, MessageSenderJson } from '../../../../../core/models/message-model/message-model';
+import {
+  MessageJson,
+  MessageSenderJson,
+} from '../../../../../core/models/message-model/message-model';
 import { UserService } from '../../../../../core/services/user-services/user-service';
 import { User, UserMessage } from '../../../../../core/models/user-models/user.model';
 import { MessageService } from '../../../../../core/services/message-service/message-service';
@@ -12,34 +15,24 @@ import { MessageService } from '../../../../../core/services/message-service/mes
   styleUrl: './messages-component.css',
 })
 export class MessagesComponent {
-  
   activatedRoute = inject(ActivatedRoute);
-  userName:string = ''
   messageService = inject(MessageService);
-  user:User | undefined
-  messages = signal<MessageSenderJson[]>([])
-
-
+  user: User | undefined;
+  messages = signal<MessageSenderJson[]>([]);
 
   constructor() {
     effect(() => {
-    this.messages = this.activatedRoute.snapshot.data['messages'];
-    this.user = this.activatedRoute.snapshot.data['messages'][0]?.sender;
+      this.messages.set(this.activatedRoute.snapshot.data['messages']);
+      this.user = this.activatedRoute.snapshot.data['messages'][0]?.sender;
     });
   }
 
-  
-//delete message function to be implemented
-delete(messageId: number)
-{
-  this.messageService.deleteMessage(messageId).subscribe({
-    next: () =>
-      {
-        this.messages.update(list => list.filter(m => m.id !== messageId));
+  //delete message function to be implemented
+  delete(messageId: number) {
+    this.messageService.deleteMessage(messageId).subscribe({
+      next: () => {
+        this.messages.update((list) => list.filter((m) => m.id !== messageId));
       },
-  
-  }) 
-}
-
- 
+    });
+  }
 }
