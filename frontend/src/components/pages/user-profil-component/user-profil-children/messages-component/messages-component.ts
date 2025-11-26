300a6ad47493e4fa928f4e0903680490c6a8ed7d
Gthierry <thierrygillot@live.com>
2025-11-26 09:33:41 +0100

    feat(messages): enhance message component with recipient dropdown and subject field

--- frontend/src/components/pages/user-profil-component/user-profil-children/messages-component/messages-component.ts
+++ frontend/src/components/pages/user-profil-component/user-profil-children/messages-component/messages-component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {
   MessageJson,
@@ -10,18 +10,24 @@ import { MessageService } from '../../../../../core/services/message-service/mes
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../../../../core/services/authentification/auth-service';
 import { time } from 'node:console';
+import { ContactService } from '../../../../../core/services/contact-service/contact-service';
+import { Contact } from '../../../../../core/models/contact-models/contact.model';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-messages-component',
-  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './messages-component.html',
-  styleUrl: './messages-component.css',
+  styleUrls: ['./messages-component.css'],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
 })
 export class MessagesComponent implements OnInit {
   //injections activated route to use with resolver and message service
   activatedRoute = inject(ActivatedRoute);
   messageService = inject(MessageService);
   authService = inject(AuthService);
+  contactService = inject(ContactService);
+  userService = inject(UserService);
   //form builder injection for message form sending
   fb = inject(FormBuilder);
   messageForm: FormGroup;
@@ -32,6 +38,10 @@ export class MessagesComponent implements OnInit {
 
   //Signal current user
   currentUser = signal<User | null>(null);
+
+  userContacts: User[] = [];
+  //contacts
+  contacts: Contact[] | undefined;
 
   //signal to control message display
   messageToDisplay = signal<boolean>(false);
@@ -45,22 +55,61 @@ export class MessagesComponent implements OnInit {
       } else {
         this.messageToDisplay.set(false);
       }
+      //set current user signal
+      this.currentUser.set(this.authService.getCurrentUser());
+
+      console.log('Current User in Messages Component:', this.currentUser());
+      console.log('---------------------------------------------');
+      console.log('Loading contacts for current user...');
+      //recupération des contacts de l'utilisateur courant
+      if (this.currentUser()) {
+        //récupération des contacts
+        console.log('Current User ID:', this.currentUser()!.user_id);
+        this.contactService.getAllContactsForUser(this.currentUser()!).subscribe((response) => {
+          console.log('Contacts response:', response);
+          this.contacts = response;
+
+          if (this.contacts) {
+            for (let c of this.contacts) {
+              this.userService.getUserById(c.contact_user_id).subscribe({
+                next: (response) => {
+                  this.userContacts.push(response);
+                },
+              });
+            }
+          }
+        });
+      }
     });
+
     //initialize the message form
     this.messageForm = this.fb.group({
       recipient: [''],
       subject: [''],
       body: [''],
     });
-    //set current user signal
-    this.currentUser.set(this.authService.getCurrentUser());
   }
 
+  //used for toggling the contacts list display in html
+  displayContactsListe = signal<boolean>(false);
+  FilteredContacts: User[] = [];
+
   ngOnInit(): void {
-    // Example: subscribe to changes in the recipient field of the form
+    //subscribe to changes in the recipient field of the form
     this.messageForm.get('recipient')?.valueChanges.subscribe((value) => {
-      console.log('Recipient changed to:', value);
+      console.log('Recipient field changed:', value);
+      this.FilteredContacts = this.userContacts.filter((user) =>
+        user.username.toLowerCase().startsWith(value.toLowerCase()),
+      );
+      console.log('Filtered Contacts:', this.FilteredContacts);
+      this.displayContactsListe.set(this.FilteredContacts.length > 0);
+      console.log('Display Contacts List:', this.displayContactsListe());
     });
+  }
+
+  selectRecipient(username: string): void {
+    this.messageForm.get('recipient')?.setValue(username);
+    this.displayContactsListe.set(false);
   }
 
   //delete message function to be implemented
@@ -71,6 +120,7 @@ export class MessagesComponent implements OnInit {
       },
     });
   }
+
   //send message function to be implemented
   sendMessage() {
     if (this.messageForm.valid && this.currentUser()) {
--- frontend/src/core/services/user-services/user-service.ts
+++ frontend/src/core/services/user-services/user-service.ts
@@ -12,6 +12,9 @@ export class UserService {
   // Inject HttpClient
   private httpClient = inject(HttpClient);
 
+  //Signal to hold list of users for the contacts
+  ContactsUserSignal = signal<User[]>([]);
+
   //get a list of users from the backend
   getAllUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl);
