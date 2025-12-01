40cdb39052b26868288996a47bc283f95459c19b
Gthierry <thierrygillot@live.com>
2025-12-01 13:57:50 +0100

    feat(tests): add commented-out mock test for retrieving all users

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -20,3 +20,35 @@ def test_get_all_users(test_client):
     assert response.status_code == 200
     data = response.get_json()
     assert isinstance(data, list)
+
+
+# def test_get_all_users(mock_get_all_users, test_client):
+#     # Créer des objets User mockés
+#     user1 = MagicMock()
+#     user1.user_id = 1
+#     user1.user_username = "john_doe"
+#     user1.user_email = "john@example.com"
+#     user1.user_first_name = "John"
+#     user1.user_last_name = "Doe"
+
+#     user2 = MagicMock()
+#     user2.user_id = 2
+#     user2.user_username = "jane_doe"
+#     user2.user_email = "jane@example.com"
+#     user2.user_first_name = "Jane"
+#     user2.user_last_name = "Doe"
+
+#     mock_get_all_users.return_value = [user1, user2]
+
+#     # Appel à l'endpoint
+#     response = test_client.get("/users")
+
+#     # Assertions
+#     assert response.status_code == 200
+#     data = response.get_json()
+#     assert len(data) == 2
+#     assert data[0]["user_username"] == "john_doe"
+#     assert data[1]["user_username"] == "jane_doe"
+
+#     # Vérifier que le service a bien été appelé
+#     mock_get_all_users.assert_called_once()
