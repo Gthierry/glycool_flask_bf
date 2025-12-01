fc8f0db8f6b15d690a27fccdc6eaf4f54a7b8bd7
Gthierry <thierrygillot@live.com>
2025-12-01 21:51:03 +0100

    feat(user-profil): implement image upload functionality with file selection handling

--- frontend/src/components/pages/user-profil-component/user-profil-component.ts
+++ frontend/src/components/pages/user-profil-component/user-profil-component.ts
@@ -4,7 +4,6 @@ import { TokenService } from '../../../core/services/token-service/token-service
 import { Router, RouterOutlet, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { filter } from 'rxjs/operators';
 import { AuthService } from '../../../core/services/authentification/auth-service';
 import { jwtDecode } from 'jwt-decode';
 
@@ -49,4 +48,38 @@ export class UserProfilComponent {
       this.route.navigate(['register']);
     }
   }
+
+  //Click on the button charger depuis le pc
+  loadImage(image: HTMLInputElement): void {
+    console.log('Load an image');
+    //on click of the button, trigger on the input click onFilesSelected($event)
+    image.click();
+  }
+
+  //list the type filelist
+  selectedFiles: File | null = null;
+  //event on the input to select file
+  onFilesSelected(event: Event): void {
+    //cible l'élément html input
+    const input = event.target as HTMLInputElement;
+
+    //si file selectionnée
+    if (!input.files || input.files.length == 0) {
+      //pas de file, on stoppe
+      return;
+    }
+    //sinon on place le fichier selectionner
+    else if (input.files && input.files.length > 0) {
+      this.selectedFiles = input.files[0];
+    }
+
+    if (this.selectedFiles) {
+      if (this.user) {
+        this.user.avatar = `profile\\${this.selectedFiles.name}`;
+      }
+    }
+  }
+
+  //copier un fichier
+  fileCopy(file: File): void {}
 }
